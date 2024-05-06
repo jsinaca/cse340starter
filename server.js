34e6dce88fb93e749bcff01a0a7b70dcc9e63b38
JosueSinaca <sinaca56@gmail.com>
2024-05-05 20:56:12 -0600

    week-3

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const router = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,7 +26,10 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 // Index route
-app.get("/",function(req,res){res.render("index", {title: "Home"})})
+// app.get("/",function(req,res){res.render("index", {title: "Home"})})
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
