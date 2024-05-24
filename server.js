9fdb58477cdb8f91881014de1113c6f78d5ae6d0
JosueSinaca <sinaca56@gmail.com>
2024-05-23 22:09:35 -0600

    Week-5 learning activity

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const session = require("express-session")
 const pool = require('./database/')
 const account=require('./routes/accountRoute')
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+
 
 /* ***********************
  * Middleware
@@ -34,6 +36,7 @@ app.use(session({
 }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser())
 
 // Express Messages Middleware
 app.use(require('connect-flash')())
@@ -41,6 +44,8 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(utilities.checkJWTToken)
 
 /* ***********************
  * View Engine and Templates
